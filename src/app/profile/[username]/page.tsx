202e278b9749d0325da1da35ddd0dc5ae4af0bbf
Harsh <126787857+Harshp2405@users.noreply.github.com>
2025-06-29 18:24:02 +0530

    Fix type error

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -3,40 +3,17 @@ import React from 'react'
 import ProfilePageClient from './ProfilePageClient';
 import { notFound } from 'next/navigation';
 
-// const ProfilePageServer = async ({ params }: { params: { username: string } }) => {
-
-//     const user = await getProfileByUsername(params.username);
-
-//     if (!user) return notFound();
-
-//     const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
-//         getUserPosts(user.id),
-//         getUserLikedPosts(user.id),
-//         isFollowing(user.id),
-//     ]);
-//     return (
-//         <div>
-//             <ProfilePageClient
-//                 user={user}
-//                 posts={posts}
-//                 likedPosts={likedPosts}
-//                 isFollowing={isCurrentUserFollowing}
-//             />
-//         </div>
-//     )
-// }
-
-// export default ProfilePageServer
-
-
 type Props = {
-    params: {
-        username: any;
-    };
+    params: Promise<{
+        username: string;
+    }>;
 };
 
 const ProfilePageServer = async ({ params }: Props) => {
-    const user = await getProfileByUsername(params.username);
+    // Await the params since it's now a Promise
+    const { username } = await params;
+
+    const user = await getProfileByUsername(username);
     if (!user) return notFound();
 
     const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
